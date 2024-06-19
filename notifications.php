dcf56bcf28caccbe076b85d187c11ae491fc6c14
Lim Jian Hang <1211202523@student.mmu.edu.my>
2024-06-19 23:33:43 +0800

    hhe

--- notifications.php
+++ notifications.php
@@ -1,8 +1,7 @@
 <?php
 include "connect.php";
 
-
-$sql = "SELECT comp_title, comp_details FROM comp";
+$sql = "SELECT comp_id, comp_title, comp_details FROM comp";
 $result = $connect->query($sql);
 ?>
 
@@ -26,18 +25,31 @@ $result = $connect->query($sql);
             <h1>Notifications</h1>
         </header>
         <main>
-            <ul>
-                <?php
-                if ($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()) {
-                        echo "<li>" . $row["comp_title"]. ": " . $row["comp_details"]. "</li>";
+            <table>
+                <thead>
+                    <tr>
+                        <th>Complaint ID</th>
+                        <th>Title</th>
+                        <th>Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if ($result->num_rows > 0) {
+                        while($row = $result->fetch_assoc()) {
+                            echo "<tr>";
+                            echo "<td>" . $row["comp_id"] . "</td>";
+                            echo "<td>" . $row["comp_title"] . "</td>";
+                            echo "<td>" . $row["comp_details"] . "</td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='3'>No complaints found.</td></tr>";
                     }
-                } else {
-                    echo "0 results";
-                }
-                $connect->close();
-                ?>
-            </ul>
+                    $connect->close();
+                    ?>
+                </tbody>
+            </table>
         </main>
     </div>
 </body>
